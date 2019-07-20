44e325446d734adce1c6a06f03ea74e8322f75ac
Mukul Jain <jainmukul1996@gmail.com>
2019-07-20 23:20:54 +0530

    cluster management with pm2

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster')
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
 
-if(cluster.isMaster){
-  cluster.fork();
-}else{
-  const express = require("express");
-  const app = express();
-  const crypto = require("crypto");
-
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("Hi there");
-    });
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("Hi there");
   });
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
