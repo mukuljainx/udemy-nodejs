8f31ae203a9e26e86d4508a3c0ee8e30e0cfdd0e
Mukul Jain <jainmukul1996@gmail.com>
2019-07-20 23:37:53 +0530

    worker thread

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 const crypto = require("crypto");
+const Worker = require("webworker-threads").Worker;
 
-app.get("/", (req, res) => {
+app.get("/pbkdf2", (req, res) => {
   crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
     res.send("Hi there");
   });
+});
+
+app.get("/worker-thread", (req, res) => {
+  const worker = new Worker(function() {
+    this.onmessage = () => {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+      this.postMessage(counter);
+    };
+  });
+
+  worker.onmessage = message => {
+    res.send("" + message.data);
+  };
+
+  worker.postMessage();
 });
 
 app.listen(3000);
