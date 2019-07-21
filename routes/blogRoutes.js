060bf695e6f13fca0c19d0f841c4d90dee285ab1
Mukul Jain <jainmukul1996@gmail.com>
2019-07-21 16:18:13 +0530

    cache every query using redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,25 +14,19 @@ module.exports = app => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    const redis = require("redis");
-    const redisUrl = "redis://127.0.0.1:6379";
-    const client = redis.createClient(redisUrl);
-    const util = require("util");
-    client.get = util.promisify(client.get);
-
     // Check if redis has cahced data
-    const cahcedBlogs = await client.get(req.user.id);
+    // const cahcedBlogs = await client.get(req.user.id);
 
-    if (cahcedBlogs) {
-      console.log("serving from cahce");
-      res.send(JSON.parse(cahcedBlogs));
-      return;
-    }
+    // if (cahcedBlogs) {
+    //   console.log("serving from cahce");
+    //   res.send(JSON.parse(cahcedBlogs));
+    //   return;
+    // }
 
     const blogs = await Blog.find({ _user: req.user.id });
 
-    console.log("serving from DB");
-    client.set(req.user.id, JSON.stringify(blogs));
+    // console.log("serving from DB");
+    // client.set(req.user.id, JSON.stringify(blogs));
 
     res.send(blogs);
   });
--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,36 @@
 const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const execQuery = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log("CUSTOM");
-  return execQuery.apply(this, arguments);
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+
+  // see if cached
+  const cachedData = await client.get(key);
+
+  if (cachedData) {
+    const doc = JSON.parse(cachedData);
+
+    if (Array.isArray(doc)) {
+      return doc.map(item => this.model(item));
+    } else {
+      return new this.model(doc);
+    }
+  }
+
+  const result = await execQuery.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };
